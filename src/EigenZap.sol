95c7c5491b09996f7ad1dcd60bfaad65355d37dc
0xClandestine <clandestine.eth@proton.me>
2023-12-12 18:04:29 -0600

    🎉 Initial commit

---
+++ src/EigenZap.sol
@@ -0,0 +1,194 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/**
+ * @title EigenZap
+ * @dev Facilitates seamless fund transfers into Lido and Rocket Pool
+ *      to acquire EigenLayer shares.
+ */
+contract EigenZap {
+    // ------------------------------------------------------------------------
+    // Immutables
+    // ------------------------------------------------------------------------
+
+    /// @notice Immutable reference to the StrategyManager contract.
+    StrategyManager public immutable manager;
+
+    /// @notice Immutable reference to the stETH contract.
+    stETH public immutable stEth;
+
+    /// @notice Immutable reference to the rETH contract.
+    address public immutable rEth;
+
+    /// @notice Immutable reference to the Lido strategy contract.
+    address public immutable lidoStrategy;
+
+    /// @notice Immutable reference to the Rocket strategy contract.
+    address public immutable rocketStrategy;
+
+    /// @notice Immutable reference to the RocketDepositPool contract.
+    RocketDepositPool public immutable rocketDepositPool;
+
+    /// @notice Immutable reference to the RocketDAOProtocolSettingsDeposit contract.
+    RocketDAOProtocolSettingsDeposit public immutable rocketSettingsDeposit;
+
+    // ------------------------------------------------------------------------
+    // Construction
+    // ------------------------------------------------------------------------
+
+    /**
+     * @dev Constructor initializes the contract with immutable references to various contracts.
+     * @param _manager The StrategyManager contract address.
+     * @param _stEth The stETH contract address.
+     * @param _rEth The rETH contract address.
+     * @param _lidoStrategy The Lido strategy contract address.
+     * @param _rocketStrategy The Rocket strategy contract address.
+     * @param _rocketDepositPool The RocketDepositPool contract address.
+     * @param _rocketSettingsDeposit The RocketDAOProtocolSettingsDeposit contract address.
+     */
+    constructor(
+        StrategyManager _manager,
+        stETH _stEth,
+        address _rEth,
+        address _lidoStrategy,
+        address _rocketStrategy,
+        RocketDepositPool _rocketDepositPool,
+        RocketDAOProtocolSettingsDeposit _rocketSettingsDeposit
+    ) {
+        manager = _manager;
+        stEth = _stEth;
+        rEth = _rEth;
+        rocketDepositPool = _rocketDepositPool;
+        lidoStrategy = _lidoStrategy;
+        rocketStrategy = _rocketStrategy;
+        rocketSettingsDeposit = _rocketSettingsDeposit;
+
+        // Approve maximum allowance for spending stETH and rETH by the manager contract.
+        ERC20Approve(address(_stEth)).approve(
+            address(_manager), type(uint256).max
+        );
+        ERC20Approve(_rEth).approve(address(_manager), type(uint256).max);
+    }
+
+    // ------------------------------------------------------------------------
+    // Actions
+    // ------------------------------------------------------------------------
+
+    /**
+     * @notice Facilitates the transfer of funds into Lido to acquire EigenLayer shares.
+     * @param expiry The expiration timestamp for the transaction.
+     * @param signature The signature for the transaction.
+     */
+    function zapIntoLido(uint256 expiry, bytes memory signature)
+        external
+        payable
+    {
+        // 1) Deposit ETH into Lido to receive stETH.
+        stEth.submit{value: msg.value}(address(0));
+
+        // 2) Deposit stETH into the strategy to receive EigenLayer shares.
+        manager.depositIntoStrategyWithSignature(
+            lidoStrategy,
+            address(stEth),
+            msg.value,
+            msg.sender,
+            expiry,
+            signature
+        );
+    }
+
+    /**
+     * @dev Facilitates the transfer of funds into RocketPool to acquire EigenLayer shares.
+     * @param expiry The expiration timestamp for the transaction.
+     * @param signature The signature for the transaction.
+     */
+    function zapIntoRocketPool(uint256 expiry, bytes memory signature)
+        external
+        payable
+    {
+        // 1) Deposit ETH into RocketPool to receive rETH.
+        rocketDepositPool.deposit{value: msg.value}();
+
+        // 2) Deposit RocketDepositPool into the strategy to receive EigenLayer shares.
+        manager.depositIntoStrategyWithSignature(
+            rocketStrategy,
+            address(rEth),
+            msg.value * rocketSettingsDeposit.getDepositFee() / 1e18,
+            msg.sender,
+            expiry,
+            signature
+        );
+    }
+
+    /**
+     * @dev Computes the digest for a given strategy deposit, used for signature verification.
+     * @param strategy The strategy contract address.
+     * @param token The token address.
+     * @param amount The deposit amount.
+     * @param nonce The transaction nonce.
+     * @param expiry The expiration timestamp for the transaction.
+     * @return The computed digest.
+     */
+    function computeDigest(
+        address strategy,
+        address token,
+        uint256 amount,
+        uint256 nonce,
+        uint256 expiry
+    ) external view returns (bytes32) {
+        return keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                manager.DOMAIN_SEPARATOR(),
+                keccak256(
+                    abi.encode(
+                        keccak256(
+                            "Deposit(address strategy,address token,uint256 amount,uint256 nonce,uint256 expiry)"
+                        ),
+                        strategy,
+                        token,
+                        amount,
+                        nonce,
+                        expiry
+                    )
+                )
+            )
+        );
+    }
+}
+
+abstract contract StrategyManager {
+    function depositIntoStrategyWithSignature(
+        address strategy,
+        address token,
+        uint256 amount,
+        address staker,
+        uint256 expiry,
+        bytes memory signature
+    ) external virtual returns (uint256 shares);
+
+    function DOMAIN_SEPARATOR() external view virtual returns (bytes32);
+}
+
+abstract contract stETH {
+    function submit(address referral)
+        external
+        payable
+        virtual
+        returns (uint256);
+}
+
+abstract contract ERC20Approve {
+    function approve(address spender, uint256 value)
+        external
+        virtual
+        returns (bool);
+}
+
+abstract contract RocketDepositPool {
+    function deposit() external payable virtual;
+}
+
+abstract contract RocketDAOProtocolSettingsDeposit {
+    function getDepositFee() external view virtual returns (uint256);
+}
---
+++ test/EigenZap.t.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/EigenZap.sol";
+
+contract EigenZapTest is Test {
+    StrategyManager constant manager =
+        StrategyManager(0x858646372CC42E1A627fcE94aa7A7033e7CF075A);
+    stETH constant stEth = stETH(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
+    address constant rEth = 0xae78736Cd615f374D3085123A210448E74Fc6393;
+    address constant lidoStrategy = 0x93c4b944D05dfe6df7645A86cd2206016c51564D;
+    address constant rocketStrategy = 0x1BeE69b7dFFfA4E2d53C2a2Df135C388AD25dCD2;
+    RocketDepositPool constant rocketDepositPool =
+        RocketDepositPool(0xDD3f50F8A6CafbE9b31a427582963f465E745AF8);
+    RocketDAOProtocolSettingsDeposit constant rocketSettingsDeposit =
+    RocketDAOProtocolSettingsDeposit(0xac2245BE4C2C1E9752499Bcd34861B761d62fC27);
+
+    EigenZap zap;
+
+    function setUp() public {
+        vm.selectFork(vm.createFork("https://eth.llamarpc.com", 17480446));
+
+        zap = new EigenZap(
+            manager,
+            stEth,
+            rEth,
+            lidoStrategy,
+            rocketStrategy,
+            rocketDepositPool,
+            rocketSettingsDeposit
+        );
+
+        vm.label(address(zap), "zap");
+        vm.label(vm.addr(420), "signer");
+        vm.label(address(this), "test");
+        vm.label(address(manager), "manager");
+        vm.label(address(stEth), "stEth");
+        vm.label(rEth, "rEth");
+        vm.label(lidoStrategy, "lidoStrategy");
+        vm.label(rocketStrategy, "rocketStrategy");
+        vm.label(address(rocketDepositPool), "rocketDepositPool");
+        vm.label(address(rocketSettingsDeposit), "rocketSettingsDeposit");
+    }
+
+    function testZapIntoLido() public {
+        uint256 key = 420;
+        address signer = vm.addr(key);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            key,
+            zap.computeDigest(
+                address(lidoStrategy),
+                address(stEth),
+                1 ether,
+                0,
+                block.timestamp
+            )
+        );
+
+        vm.deal(signer, 1 ether);
+        vm.startPrank(vm.addr(key));
+        zap.zapIntoLido{value: 1 ether}(
+            block.timestamp, abi.encodePacked(r, s, v)
+        );
+    }
+
+    function testZapIntoRocketPool() public {
+        uint256 key = 420;
+        address signer = vm.addr(key);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            key,
+            zap.computeDigest(
+                address(rocketStrategy),
+                address(rEth),
+                1 ether * rocketSettingsDeposit.getDepositFee() / 1e18,
+                0,
+                block.timestamp
+            )
+        );
+
+        vm.deal(signer, 1 ether);
+        vm.startPrank(signer);
+
+        zap.zapIntoRocketPool{value: 1 ether}(
+            block.timestamp, abi.encodePacked(r, s, v)
+        );
+    }
+}
